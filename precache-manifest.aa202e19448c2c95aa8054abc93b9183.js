ed04c6f9b3c2ef1d06ffa122672c81cdbfd752fd
SeriousHulk <50900609+SeriousHulk@users.noreply.github.com>
2020-12-30 18:18:20 +0530

    Update

---
+++ precache-manifest.aa202e19448c2c95aa8054abc93b9183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9049eb83e690fb90e47a98be73dd68",
+    "url": "/ImageGallery/index.html"
+  },
+  {
+    "revision": "fc2849501344ddb3a3fd",
+    "url": "/ImageGallery/static/css/main.52e2871a.chunk.css"
+  },
+  {
+    "revision": "c98cf3c44f6cd9f12e04",
+    "url": "/ImageGallery/static/js/2.5f38b4b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ImageGallery/static/js/2.5f38b4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2849501344ddb3a3fd",
+    "url": "/ImageGallery/static/js/main.e9dd8c07.chunk.js"
+  },
+  {
+    "revision": "8821d546c7414b5eaaff",
+    "url": "/ImageGallery/static/js/runtime-main.49271329.js"
+  }
+]);
